abbef5730f51ccb2397444676e964595d3199f6d
fletchervmiles <98453784+fletchervmiles@users.noreply.github.com>
2024-07-12 10:05:13 +0700

    Updating Slug

--- test10.py
+++ test10.py
@@ -458,7 +458,7 @@ async def make_outgoing_call():
             'ncco': [
                 {
                     'action': 'record',
-                    'eventUrl': [f'https://e1d1-138-199-40-175.ngrok-free.app/vonage_recording?call_id={call_id}'],
+                    'eventUrl': [f'https://franko-06.onrender.com/vonage_recording?call_id={call_id}'],
                     'format': 'mp3'
                 },
                 {
@@ -466,7 +466,7 @@ async def make_outgoing_call():
                     'endpoint': [
                         {
                             'type': 'websocket',
-                            'uri': f'wss://e1d1-138-199-40-175.ngrok-free.app/ws?call_id={call_id}',
+                            'uri': f'wss://franko-06.onrender.com/ws?call_id={call_id}',
                             'content-type': 'audio/l16;rate=16000',
                             'headers': {
                                 'language': 'en-GB',
@@ -476,7 +476,7 @@ async def make_outgoing_call():
                     ]
                 }
             ],
-            'event_url': [f'https://e1d1-138-199-40-175.ngrok-free.app/vonage_call_status?call_id={call_id}'],
+            'event_url': [f'https://franko-06.onrender.com/vonage_call_status?call_id={call_id}'],
             'event_method': 'POST'
         })
 
